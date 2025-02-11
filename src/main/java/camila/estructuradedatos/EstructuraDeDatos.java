69468fe19b1f35760a580548f116e88c323decc2
Camila Escobedo <14523@ulsachihuahua.edu.mx>
2025-02-11 10:28:30 -0600

    anadi StudentGrades como un archivo nuevo

--- src/main/java/camila/estructuradedatos/EstructuraDeDatos.java
+++ src/main/java/camila/estructuradedatos/EstructuraDeDatos.java
@@ -11,17 +11,29 @@ package camila.estructuradedatos;
 public class EstructuraDeDatos {
 
     public static void main(String[] args) {
-        Promedio objPromedio= new Promedio();
-        objPromedio.calcularPromedio();
         
-        Names objNames = new Names();
-        objNames.showNames();
+        //Promedio objPromedio= new Promedio();
+        //objPromedio.calcularPromedio();
         
-        Juego objJuego = new Juego();
-        objJuego.lanzarJuego();
+        //Names objNames = new Names();
+        //objNames.showNames();
+        
+        //Juego objJuego = new Juego();
+        //objJuego.lanzarJuego();
                 
-        UserData objUserData = new UserData();
-        String fullName = objUserData.fullName("Camila", "Escobedo");
-        System.out.println(fullName);           
+        //UserData objUserData = new UserData();
+        //String fullName = objUserData.fullName("Camila", "Escobedo");
+        //System.out.println(fullName);        
+        
+        StudentGrades objStudentGrades = new StudentGrades();
+        int firstPartialGrade = objStudentGrades.calculateFirstPartialGrade(85, 90);
+        System.out.println("Calificacion del parcial: " + firstPartialGrade);
+        
+        int totalFinal = objStudentGrades.calculateFinalGrade(80, 75, 90, 85);
+        System.out.println("Calificacion final del semestre: " + totalFinal);
+        
+        String attendanceStatus = objStudentGrades.checkFailureByAbsences(64, 32);
+        System.out.println("Porcentaje de faltas: %" + attendanceStatus);
     }
+    
 }
---
+++ src/main/java/camila/estructuradedatos/StudentGrades.java
@@ -0,0 +1,43 @@
+package camila.estructuradedatos;
+
+/**
+ * @author:CamilaEscobedo
+ * @matricula:14523
+ * @fecha:2/11/2025
+ */
+
+//se crea la clase StudentGrades
+public class StudentGrades {
+    //se crea el metodo calculateFirstPartialGrade donde se espera recibir dos parametros
+    public int calculateFirstPartialGrade(int examScore, int homeworkScore) {
+        //se calcula la primer calificacion parcial en double para redondear
+        double firstPartialGrade= (examScore * 0.7 + homeworkScore * 0.3);
+        //se convierte de double a int para devolver un numero entero
+        return (int)(firstPartialGrade);   
+    }
+    
+    //se crea el metodo calculateFinalGrade donde se espera recibir cuatro parametros
+    public int calculateFinalGrade(int firstPartial, int secondPartial, int thirdPartial, int finalExam) {
+        //se calcula el promedio de los tres parciales en double para redondear
+        double promPartial = ((double) firstPartial+secondPartial+thirdPartial)/3;
+        //se calcula el porcentaje del examen final y el de los parciales y se suman en double para redondear
+        double totalFinal = (finalExam * 0.5 + promPartial * 0.5);
+        //se convierte de double a int para devolver un numero entero
+        return (int)(totalFinal);
+    }
+    
+    //se crea el metodo checkFailureByAbsences donde se espera recibir dos parametros
+    public String checkFailureByAbsences(int totalHours, int absences) {
+        //se calcula el porcentaje de faltas en double para redondear
+        double attendanceStatus = ((double) absences / totalHours) * 100;
+        //se usa if and else para determinar si el alumno esta reprobrado o aprobado
+        if (attendanceStatus > 10.0) {
+        System.out.println("Alumno reprobado por faltas");}
+        else {
+        System.out.println("Alumno cumple con las asistencias minimas");}
+        //se convierte de double a String para devolver una cadena
+        return Double.toString(attendanceStatus);
+    }
+    
+}
+    
