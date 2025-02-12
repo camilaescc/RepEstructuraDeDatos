ef2a9f352161a874c3810f95658b2ada6f6f16cd
Camila Escobedo <14523@ulsachihuahua.edu.mx>
2025-02-12 10:49:34 -0600

    se formatea el codigo

--- src/main/java/camila/estructuradedatos/CircleArea.java
+++ src/main/java/camila/estructuradedatos/CircleArea.java
@@ -5,14 +5,14 @@ package camila.estructuradedatos;
  * @matricula:14523
  * @fecha:2/12/2025
  */
-
 //se crea la clase CircleArea
 public class CircleArea {
+
     //se crea el metodo calculateArea en double donde recibe un parametro radius
     public double calculateArea(double radius) {
         //regresa en double la operacion que daria como resultado el area de un circulo
         //se usa el de la libreria de java Math para realizar las operaciones correctamente
-        return (Math.PI)*Math.pow(radius, 2);
+        return (Math.PI) * Math.pow(radius, 2);
     }
-    
+
 }
--- src/main/java/camila/estructuradedatos/TriangleArea.java
+++ src/main/java/camila/estructuradedatos/TriangleArea.java
@@ -5,13 +5,13 @@ package camila.estructuradedatos;
  * @matricula:14523
  * @fecha:2/12/2025
  */
-
 //se crea la clase TriangleArea
 public class TriangleArea {
+
     //se crea el metodo calculateArea en double y recibe dos paramatros
     public double calculateArea(double base, double height) {
         //regresa la operacion que daria como resultado el area de un triangulo en double
-        return (base*height)/2;
+        return (base * height) / 2;
     }
-    
+
 }
