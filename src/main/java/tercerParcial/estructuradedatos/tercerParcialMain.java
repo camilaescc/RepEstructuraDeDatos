3b8a77db9bd38f66b8a43ee2d940687ecfbce6b4
Camila Escobedo <14523@ulsachihuahua.edu.mx>
2025-04-08 10:29:56 -0600

    se agrega archivo de tercer parcial

---
+++ src/main/java/tercerParcial/estructuradedatos/tercerParcialMain.java
@@ -0,0 +1,22 @@
+package tercerParcial.estructuradedatos;
+
+public class tercerParcialMain {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+
+        System.out.println("List:");
+        list.display();
+
+        System.out.println("Is 20 in the list? " + list.search(20));
+
+        list.delete(20);
+
+        System.out.println("After deleting 20:");
+        list.display();
+    }
+}
+
